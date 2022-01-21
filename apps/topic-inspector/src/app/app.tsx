73e6660bede6f3f716542e544d7a2d61dc652ebc
Finn <34718806+FinnDore@users.noreply.github.com>
2022-01-21 14:02:22 +0000

    initial commit

---
+++ apps/topic-inspector/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="topic-inspector" />
+      <div />
+    </>
+  );
+}
+
+export default App;
