86363dd850f8487c5cf0021e6052670728bec8f0
Finn <34718806+FinnDore@users.noreply.github.com>
2022-01-21 21:50:17 +0000

    Added a basic  navbar.

--- apps/topic-inspector/src/app/app.tsx
+++ apps/topic-inspector/src/app/app.tsx
@@ -1,9 +1,39 @@
-import { Paper } from '@mui/material';
+import { Button, IconButton, MenuItem, Paper } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import InspectTopics from './features/inspect-topics/inspect-topics';
 import { RootState } from './_store/store';
 import classes from './app.module.scss';
+import GITHUB_MARK_DARK from '../assets/github-mark-dark.png';
+const GITHUB_LINK = 'https://github.com/FinnDore/topic-inspector';
+
+function Header() {
+    return (
+        <header className={classes['header']}>
+            <h2>Topic Inspector</h2>
+            <div className={classes['button-row']}>
+                <Link to="/">
+                    <Button>Inspect Topic</Button>
+                </Link>
+                <Link to="/help">
+                    <Button>Help</Button>
+                </Link>
+                <Link to="/about">
+                    <Button>About</Button>
+                </Link>
+                <Button
+                    className={classes['icon-button']}
+                    color="primary"
+                    aria-label="Goto topic inspectors github"
+                >
+                    <a href={GITHUB_LINK}>
+                        <img src={GITHUB_MARK_DARK} alt="Github mark"></img>
+                    </a>
+                </Button>
+            </div>
+        </header>
+    );
+}
 
 export function App() {
     const selectKafkaLogDirs = useSelector(
@@ -15,11 +45,7 @@ export function App() {
 
     return (
         <>
-            <header className={classes['header']}>
-                <Paper variant="outlined" square>
-                    Topic Inspector
-                </Paper>
-            </header>
+            <Header></Header>
             <Route
                 path="/"
                 exact
@@ -30,6 +56,8 @@ export function App() {
                     </div>
                 )}
             />
+            <Route path="/about" exact render={() => <div>about</div>} />
+            <Route path="/help" exact render={() => <div>help</div>} />
         </>
     );
 }
--- apps/topic-inspector/src/main.tsx
+++ apps/topic-inspector/src/main.tsx
@@ -5,6 +5,11 @@ import { Provider } from 'react-redux';
 import App from './app/app';
 import { GlobalStore } from './app/_store/store';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 ReactDOM.render(
     <StrictMode>
         <Provider store={GlobalStore}>
