5202d3639b74f06d4019608ca19678407798ca59
Finn <34718806+FinnDore@users.noreply.github.com>
2022-01-28 16:12:40 +0000

    Changed useHook to useMemo; memoised some components;

---
+++ apps/topic-inspector/src/app/features/inspect-topics/tree-map/tree-leaf/tree-leaf.tsx
@@ -0,0 +1,60 @@
+import { Group } from '@visx/group';
+import {
+    HierarchyNode,
+    HierarchyRectangularNode
+} from '@visx/hierarchy/lib/types';
+import { memo } from 'react';
+import { TreeData } from '../../../../_interfaces/tree-data.model';
+import { background } from '../tree-map';
+import { TreeMapText } from './tree-map-text/tree-map-text';
+
+export interface TreeLeafProps {
+    node: HierarchyRectangularNode<HierarchyNode<HierarchyNode<TreeData>>>;
+    handleMouseOver: (
+        event: React.PointerEvent<SVGRectElement>,
+        datum: TreeData
+    ) => void;
+    margin: { top: number; left: number; right: number; bottom: number };
+    colorScale: (value: number) => string;
+}
+
+/**
+ * The tree leaf component for the tree map chart. memoised for performance
+ *
+ * @param props the input props
+ * @returns {object} the component
+ */
+export const TreeLeaf = memo(
+    ({ handleMouseOver, margin, colorScale, node }: TreeLeafProps) => {
+        const nodeWidth = node.x1 - node.x0;
+        const nodeHeight = node.y1 - node.y0;
+        const data = node.data.data as unknown as TreeData;
+
+        return (
+            <Group
+                key={data.id}
+                top={node.y0 + margin.top}
+                left={node.x0 + margin.left}
+            >
+                {node.depth > 0 && (
+                    <>
+                        <rect
+                            width={nodeWidth}
+                            height={nodeHeight}
+                            stroke={background}
+                            onPointerMove={e => handleMouseOver(e, data)}
+                            fill={colorScale(node.value ?? 0)}
+                        />
+                        <TreeMapText
+                            data={data}
+                            marginLeft={margin.left}
+                            marginTop={margin.top}
+                            nodeHeight={nodeHeight}
+                            nodeWidth={nodeWidth}
+                        ></TreeMapText>
+                    </>
+                )}
+            </Group>
+        );
+    }
+);
